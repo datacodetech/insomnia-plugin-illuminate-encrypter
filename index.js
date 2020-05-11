3dd8593c1fcccf91bc5d0bdc7f75147aec2dd40d
Jacek Kuzemczak <jacek@dataplan.co.uk>
2020-05-11 16:15:23 +0100

    Populated tag args

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ module.exports.templateTags = [
     {
         name: "illuminate_encrypt",
         displayName: "Illuminate Encrypt",
-        args: [],
-        async run (context) {
-            return "";
-        }
+        description: "Encrypt a value for Illuminate\\Encryption\\Encrypter::decrypt()",
+        args: [
+            {
+                displayName: "Encryption key",
+                description: "",
+                type: "string",
+                placeholder: "32 character key",
+            },
+            {
+                displayName: "Cipher",
+                description: "",
+                type: "enum",
+                options: [
+                    { displayName: "AES-128-CBC", value: "AES-128-CBC" },
+                    { displayName: "AES-256-CBC", value: "AES-256-CBC" },
+                ],
+            },
+            {
+                displayName: "Value",
+                description: "The value to be encrypted",
+                type: "string",
+            },
+        ],
+        async run (context, key, cipher, value) {
+            return "test";
+        },
     },
 ];
