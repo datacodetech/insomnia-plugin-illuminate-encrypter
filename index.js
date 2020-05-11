3ed88b646e0a3d296c472ab70c776127b21c7313
Jacek Kuzemczak <jacek@dataplan.co.uk>
2020-05-11 19:19:38 +0100

    Implemented encrypt function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const crypto = require("crypto");
+
 module.exports.templateTags = [
     {
         name: "illuminate_encrypt",
         displayName: "Illuminate Encrypt",
-        description: "Encrypt a value for Illuminate\\Encryption\\Encrypter::decrypt()",
+        description: "Encrypt a value for Illuminate\\Encryption\\Encrypter::decryptString()",
         args: [
             {
                 displayName: "Encryption key",
@@ -25,8 +27,32 @@ module.exports.templateTags = [
                 type: "string",
             },
         ],
-        async run (context, key, cipher, value) {
-            return "test";
+        async run (context, key, algorithm, value) {
+            // Based on https://github.com/illuminate/encryption/blob/master/Encrypter.php#L82
+            // The encrypt() method from Illuminate\Encryption\Encrypter
+
+            const iv = crypto.randomBytes(16);
+            const ivBase64 = Buffer.from(iv).toString("base64");
+
+            const cipher = crypto.createCipheriv(algorithm, key, iv)
+            const hmac = crypto.createHmac("SHA256", key)
+
+            const payload = cipher.update(value, "utf8", "base64")
+                          + cipher.final("base64");
+
+            hmac.update(`${ivBase64}${payload}`);
+            const hash = hmac.digest("hex");
+
+            const resultData = {
+                iv: ivBase64,
+                value: payload,
+                mac: hash,
+            };
+
+            const resultJson = JSON.stringify(resultData);
+            const resultJsonBase64 = Buffer.from(resultJson).toString("base64");
+
+            return resultJsonBase64;
         },
     },
 ];
